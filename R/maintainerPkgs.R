1d67d35114c00afa6b46f73490171a694cc8daf3
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-10-29 17:13:37 -0400

    update docs tibble return value

--- R/maintainerPkgs.R
+++ R/maintainerPkgs.R
@@ -17,7 +17,7 @@
 #' @importFrom BiocManager version
 #' @importFrom BiocBaseUtils isScalarCharacter
 #'
-#' @returns A `data.frame` with the list of packages maintained by the specified
+#' @returns A `tibble` with the list of packages maintained by the specified
 #'   maintainer. The data frame contains columns such as `Package`, `Version`,
 #'   `Author`, and `Maintainer`
 #'
