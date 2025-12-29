82b71804d6b9aef3c5f2f955e76142c5aaca37b4
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-12-29 15:05:50 -0500

    add empty data.frame for annotation packages

--- R/buildreport.R
+++ R/buildreport.R
@@ -1,5 +1,14 @@
 .TEST_API_URL <- "http://127.0.0.1:8000/"
 
+.SENTINEL_CHECK_RES <- data.frame(
+    pkg = character(0L),
+    author = character(0L),
+    version = character(0L),
+    git_last_commit = character(0L),
+    git_last_commit_date = character(0L),
+    pkgType = character(0L)
+)
+
 #' Bioconductor Package Build Report
 #'
 #' Queries the Bioconductor API to retrieve build reports for all packages
@@ -26,19 +35,19 @@ buildreport <- function(
         isScalarCharacter(main)
     )
     pkgs <- maintainerPkgs(main = main, version = version)[["Package"]]
-    pkgList <- lapply(
-        pkgs,
-        function(pkg) {
-            paste0(.TEST_API_URL, "checkResults/package/", pkg) |>
-                request() |>
-                req_perform() |>
-                resp_body_json(simplifyVector = TRUE)
-        }
-    )
-    return(
-        do.call(
-            what = rbind.data.frame,
-            args = pkgList
-        )
-    )
+    pkgList <- lapply(pkgs, .checkres_pkg)
+    do.call(rbind.data.frame, pkgList)
+}
+
+.checkres_pkg <- function(pkg) {
+    message("Trying package: ", pkg)
+    tryCatch({
+        paste0(.TEST_API_URL, "checkResults/package/", pkg) |>
+            request() |>
+            req_perform() |>
+            resp_body_json(simplifyVector = TRUE)
+    }, error = function(e) {
+        warning(conditionMessage(e))
+        .SENTINEL_CHECK_RES
+    })
 }
